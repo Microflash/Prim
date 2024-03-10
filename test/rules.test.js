66b04688be879a4dbaa7feef9480196015a92ee6
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2024-03-11 02:32:56 +0530

    chore(test): harness for tests

---
+++ test/rules.test.js
@@ -0,0 +1,27 @@
+import path from "path";
+import { execa } from "execa";
+import { expect, test } from "vitest";
+import scenario from "./parameters/index.js";
+
+const command = "vale";
+const args = [
+	"--output=line",
+	"--sort",
+	"--normalize",
+	"--relative",
+	"test.md"
+]
+const fixturesDirectory = "fixtures";
+
+test.each(scenario)(`Testing %s`, async (rule, expected) => {
+	let result;
+	try {
+		const cwd = path.resolve(process.cwd(), fixturesDirectory, rule);
+		const output = await execa(command, args, { cwd: cwd });
+		result = output.stdout;
+	} catch (error) {
+		result = error.stdout;
+	}
+
+	expect.soft(result.trim()).toBe(expected.trim());
+});
