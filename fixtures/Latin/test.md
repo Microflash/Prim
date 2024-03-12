260265789700b2b9f8e4d451b7be559e0a14290c
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2024-03-12 06:01:08 +0530

    feat(rule): more Latin

--- fixtures/Latin/test.md
+++ fixtures/Latin/test.md
@@ -1,8 +1,17 @@
 # Latin
 
+<!-- circa -->
+He was born circa 1600.
+
 <!-- e.g. -->
 The rocky planets (e.g., Mercury) are closest to our sun.
 Farmer Brown sells his produce (eg, apples, oranges, carrots) at the market.
+
+<!-- ergo -->
+The products are poorly constructed; ergo, they break easily.
+
+<!-- et al. -->
+Taylor et al., 2018
 
 <!-- etc. -->
 You can leave your coats, umbrellas, etc., at the door.
@@ -16,3 +25,9 @@ It'll be the Red Sox vs. the Yankees in tonight's game.
 
 <!-- versus -->
 Monitor your liabilities versus your assets.
+
+<!-- via -->
+The London to Cairo flight goes via Rome
+
+<!-- viz -->
+Our philosophy is minimalist, viz., simple lines and clean colors.
--- test/parameters/Latin/index.js
+++ test/parameters/Latin/index.js
@@ -1,12 +1,17 @@
 export default [
 	"Latin",
 	`
-	test.md:4:20:Crystal.Latin:Avoid latin. Use 'for example' instead of 'e.g.,'.
-	test.md:5:33:Crystal.Latin:Avoid latin. Use 'for example' instead of 'eg,'.
-	test.md:8:38:Crystal.Latin:Avoid latin. Use 'and other(s)' instead of 'etc.'.
-	test.md:11:20:Crystal.Latin:Avoid latin. Use 'that is' instead of 'i.e.,'.
-	test.md:12:33:Crystal.Latin:Avoid latin. Use 'that is' instead of 'ie,'.
-	test.md:15:22:Crystal.Latin:Avoid latin. Use 'against' instead of 'vs.'.
-	test.md:18:26:Crystal.Latin:Avoid latin. Use 'against' instead of 'versus'.
+	test.md:4:13:Crystal.Latin:Avoid latin. Use 'around' or 'about' instead of 'circa'.
+	test.md:7:20:Crystal.Latin:Avoid latin. Use 'for example' instead of 'e.g.,'.
+	test.md:8:33:Crystal.Latin:Avoid latin. Use 'for example' instead of 'eg,'.
+	test.md:11:38:Crystal.Latin:Avoid latin. Use 'therefore' instead of 'ergo'.
+	test.md:14:8:Crystal.Latin:Avoid latin. Use 'and others' instead of 'et al.'.
+	test.md:17:38:Crystal.Latin:Avoid latin. Use 'and other(s)' instead of 'etc.'.
+	test.md:20:20:Crystal.Latin:Avoid latin. Use 'that is' instead of 'i.e.,'.
+	test.md:21:33:Crystal.Latin:Avoid latin. Use 'that is' instead of 'ie,'.
+	test.md:24:22:Crystal.Latin:Avoid latin. Use 'against' instead of 'vs.'.
+	test.md:27:26:Crystal.Latin:Avoid latin. Use 'against' instead of 'versus'.
+	test.md:30:33:Crystal.Latin:Avoid latin. Use 'over' or 'through' instead of 'via'.
+	test.md:33:31:Crystal.Latin:Avoid latin. Use 'namely' instead of 'viz.'.
 	`
 ];
