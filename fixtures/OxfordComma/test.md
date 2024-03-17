a6b69483388a848b902ecb76c5b93b92991ca62c
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2024-03-17 15:11:47 +0530

    feat(rule): oxford comma (simple)

---
+++ fixtures/OxfordComma/test.md
@@ -0,0 +1,6 @@
+# Oxford Comma
+
+The ingredients for the recipe included flour, sugar, eggs and butter.
+<!-- He enjoys reading, playing the guitar and hiking in his free time. -->
+Should I unpack my clothes, toiletries or books?
+<!-- You can choose to visit the museum, go hiking or relax at home. -->
--- test/parameters/index.js
+++ test/parameters/index.js
@@ -3,6 +3,7 @@ import Ellipsis from "./Ellipsis/index.js";
 import EmDash from "./EmDash/index.js";
 import EnDash from "./EnDash/index.js";
 import Latin from "./Latin/index.js";
+import OxfordComma from "./OxfordComma/index.js";
 import Semicolon from "./Semicolon/index.js";
 import SentenceLength from "./SentenceLength/index.js";
 import Space from "./Space/index.js";
@@ -13,6 +14,7 @@ export default [
 	EmDash,
 	EnDash,
 	Latin,
+	OxfordComma,
 	Semicolon,
 	SentenceLength,
 	Space
